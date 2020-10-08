0cfe4e27a06a16e60db7b4474cbaa34ba40ca2fd
DAMIAN M <12damie@gmail.com>
2020-10-08 19:24:00 +0200

    Revert "add function min and max"
    This reverts commit e006e0736be6f8dc15acec1d7aad7a2a2ba7caed.

--- app.js
+++ app.js
@@ -4,16 +4,6 @@ function avg(){
     console.log("srednia:",_.mean(arguments));
 }
 
-function min(){
-    console.log("min:",_.min(arguments));
-}
-
-function max(){
-    console.log("max:",_.max(arguments));
-}
 avg(4,4,4);
 avg(5,10,5);
 avg(2,3,4,5,6);
-
-min(1,3,4,5,0,7,8,9);
-max(1,3,4,5,0,7,8,20);
